9cdfc55d81c65188d181169cb8907f5319cd5e09
Arturo Hernández <arturoaureliohr@gmail.com>
2023-08-14 23:11:12 -0400

    Remove unnecessary comments in latest migration

--- database/migrations/20230722171403_change_book_page_foreign_key_to_a_book_format_record_id.sql
+++ database/migrations/20230722171403_change_book_page_foreign_key_to_a_book_format_record_id.sql
@@ -3,11 +3,9 @@
 ALTER TABLE book_page 
 DROP CONSTRAINT fk_book_to_book_page;
 
--- Rename the column
 ALTER TABLE book_page 
 RENAME COLUMN book_id TO book_format_id;
 
--- Add the new foreign key constraint
 ALTER TABLE book_page 
 ADD CONSTRAINT fk_book_format_to_book_page 
 FOREIGN KEY(book_format_id) REFERENCES book_format(id);
@@ -18,11 +16,9 @@ FOREIGN KEY(book_format_id) REFERENCES book_format(id);
 ALTER TABLE book_page 
 DROP CONSTRAINT fk_book_format_to_book_page;
 
--- Rename the column
 ALTER TABLE book_page 
 RENAME COLUMN book_format_id TO book_id;
 
--- Add the new foreign key constraint
 ALTER TABLE book_page 
 ADD CONSTRAINT fk_book_to_book_page 
 FOREIGN KEY(book_id) REFERENCES book(id);
