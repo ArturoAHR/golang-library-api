0fedecd0ca4ab7b115a76021cecc316c0bb84e95
Arturo Hernández <arturoaureliohr@gmail.com>
2023-08-14 23:05:03 -0400

    Implement seeder script

--- scripts/seeder/main.go
+++ scripts/seeder/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"os"
 
+	seeder "github.com/ArturoAHR/golang-library-api/database/seeders"
 	"github.com/joho/godotenv"
 
 	"gorm.io/driver/postgres"
@@ -10,6 +13,8 @@ import (
 )
 
 var DB *gorm.DB
+
+type SeederFunction func(*gorm.DB) error
 
 func init() {
 	if err := godotenv.Load(); err != nil {
@@ -26,5 +31,20 @@ func init() {
 }
 
 func main() {
+	seedFunctions := []SeederFunction{
+		seeder.SeedBooks,
+		seeder.SeedFormats,
+		seeder.SeedProviders,
+		seeder.SeedBookFormats,
+		seeder.SeedBookPages,
+	}
 
+	for _, seedFunction := range seedFunctions {
+		err := seedFunction(DB)
+		if err != nil {
+			log.Fatalf("Unable to seed: %v", err)
+		}
+	}
+
+	fmt.Println("Seeding procedure completed")
 }
