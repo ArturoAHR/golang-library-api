241cd62046c4bceac578d3ed653de380466bb6f1
Arturo Hernández <arturoaureliohr@gmail.com>
2023-08-16 14:38:34 -0400

    Add database connection singleton

--- cmd/golang-library-api/main.go
+++ cmd/golang-library-api/main.go
@@ -1,28 +1,16 @@
 package main
 
 import (
-	"os"
-
+	dbconnection "github.com/ArturoAHR/golang-library-api/internal/database"
 	"github.com/joho/godotenv"
-
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
 )
-
-var DB *gorm.DB
 
 func init() {
 	if err := godotenv.Load(); err != nil {
-		panic("No .env file found")
+		panic("no .env file found")
 	}
 
-	dsn := os.Getenv("GORM_DSN")
-	tmpDB, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	if err != nil {
-		panic("failed to connect database")
-	}
-
-	DB = tmpDB
+	dbconnection.GetInstance()
 }
 
 func main() {
