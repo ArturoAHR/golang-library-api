9e115c1c45ac4e0264068d2734ccdd2f4df70234
Arturo Hernández <arturoaureliohr@gmail.com>
2023-08-16 15:15:18 -0400

    Add router in entry point as well as books module handler mapping

--- cmd/golang-library-api/main.go
+++ cmd/golang-library-api/main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"net/http"
+
 	dbconnection "github.com/ArturoAHR/golang-library-api/internal/database"
+	bookmodule "github.com/ArturoAHR/golang-library-api/internal/modules/book"
+	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
 )
 
@@ -14,5 +18,9 @@ func init() {
 }
 
 func main() {
+	router := mux.NewRouter()
+	router.PathPrefix("/book").HandlerFunc(bookmodule.Handler)
 
+	http.Handle("/", router)
+	http.ListenAndServe(":8080", nil)
 }
---
+++ internal/modules/book/book.handler.go
@@ -0,0 +1,9 @@
+package bookmodule
+
+import (
+	"net/http"
+)
+
+func Handler(writer http.ResponseWriter, request *http.Request) {
+
+}
