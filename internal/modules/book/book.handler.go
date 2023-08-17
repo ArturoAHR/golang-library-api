856a3158a23075b09e971adc5db8a7f36bd69f8e
Arturo Hernández <arturoaureliohr@gmail.com>
2023-08-16 21:21:49 -0400

    Change the structure of the endpoint to retrieve a book page

--- internal/modules/book/book.handler.go
+++ internal/modules/book/book.handler.go
@@ -20,7 +20,7 @@ func Handler(writer http.ResponseWriter, request *http.Request) {
 
 	subRouter.HandleFunc("/book", controller.getBooks).Methods("GET")
 	subRouter.HandleFunc("/book/{bookId}", controller.getBook).Methods("GET")
-	subRouter.HandleFunc("/book/{bookId}/format/{formatId}", controller.getBookPage).Methods("GET")
+	subRouter.HandleFunc("/book/version/{versionId}", controller.getBookPage).Methods("GET")
 
 	subRouter.ServeHTTP(writer, request)
 }
